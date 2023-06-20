7404a4f3a3e71e3ad4ee7f9f8d6ddb7fa59241be
hynseok <wefwef12e@naver.com>
2023-06-20 14:47:22 +0900

    init

---
+++ src/users/users.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Body, Post, Get, Param, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserInfo } from './UserInfo';
+import { userInfo } from 'os';
+import { UsersService } from './users.service';
+import { sign } from 'crypto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    const { name, email, password } = dto;
+    await this.userService.createUser(name, email, password);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
+
+    return await this.userService.verifyEmail(signupVerifyToken);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+
+    return await this.userService.login(email, password);
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
+  }
+}
